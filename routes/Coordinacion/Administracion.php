29d78b1e9582a6e1feb258a859dcc98eb9d798b0
IprodhaNewWeb <zitelli.diego.a@gmail.com>
2023-02-24 11:04:22 -0300

    ruta de administracion

--- routes/Coordinacion/Administracion.php
+++ routes/Coordinacion/Administracion.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Coordinacion\Administracion\Compras\Rubro\RubroController;
 
 
-Route::group(['middleware' => ['auth','role_or_permission:ADMIN|VER-RUBRO']], function () {
+Route::group(['middleware' => ['auth','role_or_permission:ADMIN|VER-RUBROS']], function () {
     Route::get('/rubrosxemp',  [RubroController::class, 'empresaxrubro'])->name('rubros.empresa');
     Route::get('/rubrosxemp/asignar/{id}',  [RubroController::class, 'asignarRubro'])->name('rubroxemp.asignar');
     Route::resource('rubros', RubroController::class);
