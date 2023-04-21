354433b05712bc142bcb7e3181aba6363185e100
IprodhaNewWeb <80710956+Lisi-prog@users.noreply.github.com>
2023-04-21 08:34:52 -0300

    arreglo rubros

--- routes/Planificacion/Planificacion.php
+++ routes/Planificacion/Planificacion.php
@@ -9,5 +9,6 @@ Route::group(['middleware' => ['auth','role_or_permission:ADMIN']], function () 
     Route::post('/obra/vivienda/{id}/{orden}', [ObraviviendaController::class, 'viviendaDeObra']);
     Route::get('/obravivienda/viv/{id}', [ObraviviendaController::class, 'verViv'])->name('obravivienda.viviendas');
     Route::get('/obravivienda/eta/{id}', [ObraviviendaController::class, 'verEta'])->name('obravivienda.etapas');
+    Route::get('/obravivienda/ent/{id}/{entrega}', [ObraviviendaController::class, 'asignarVivEnt'])->name('obravivienda.entrega');
     Route::resource('obravivienda', ObraviviendaController::class);
 });
