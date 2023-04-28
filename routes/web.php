6c142cf339cc0ab9c96b44ed46bb7d1833f2b275
IprodhaNewWeb <80710956+Lisi-prog@users.noreply.github.com>
2023-04-28 09:58:58 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,31 @@ use App\Http\Controllers\NotificacionController;
 |
 */
 Route::get('/ipusuario', function(Request $request){
-    echo $_SERVER['REMOTE_ADDR'];
+    $clientIps = array();
+    $ip = $this->server->get('REMOTE_ADDR');
+    if (!$this->isFromTrustedProxy()) {
+        return array($ip);
+    }
+    if (self::$trustedHeaders[self::HEADER_FORWARDED] && $this->headers->has(self::$trustedHeaders[self::HEADER_FORWARDED])) {
+        $forwardedHeader = $this->headers->get(self::$trustedHeaders[self::HEADER_FORWARDED]);
+        preg_match_all('{(for)=("?\[?)([a-z0-9\.:_\-/]*)}', $forwardedHeader, $matches);
+        $clientIps = $matches[3];
+    } elseif (self::$trustedHeaders[self::HEADER_CLIENT_IP] && $this->headers->has(self::$trustedHeaders[self::HEADER_CLIENT_IP])) {
+        $clientIps = array_map('trim', explode(',', $this->headers->get(self::$trustedHeaders[self::HEADER_CLIENT_IP])));
+    }
+    $clientIps[] = $ip; // Complete the IP chain with the IP the request actually came from
+    $ip = $clientIps[0]; // Fallback to this when the client IP falls into the range of trusted proxies
+    foreach ($clientIps as $key => $clientIp) {
+        // Remove port (unfortunately, it does happen)
+        if (preg_match('{((?:\d+\.){3}\d+)\:\d+}', $clientIp, $match)) {
+            $clientIps[$key] = $clientIp = $match[1];
+        }
+        if (IpUtils::checkIp($clientIp, self::$trustedProxies)) {
+            unset($clientIps[$key]);
+        }
+    }
+    // Now the IP chain contains only untrusted proxies and the client IP
+    return $clientIps ? array_reverse($clientIps) : array($ip);
 });
 
 Route::get('/sintaxis',function()
