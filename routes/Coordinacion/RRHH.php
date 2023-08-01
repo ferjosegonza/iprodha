32e68057f4ca5975fc28f17178817c514a3f9aa8
solmala <soledadmalazotto@gmail.com>
2023-08-01 08:23:43 -0300

    Update RRHH.php

--- routes/Coordinacion/RRHH.php
+++ routes/Coordinacion/RRHH.php
@@ -11,4 +11,9 @@ Route::group(['middleware' => ['auth','role_or_permission:ADMIN|RRHH']], functio
     Route::get('agente/buscar', [AgenteController::class, 'buscar'])->name('agente.buscar');
     Route::get('agente/historial', [AgenteController::class, 'historial'])->name('agente.historial');
     Route::get('{id}/crear_novedad', [NovedadesController::class, 'crear_novedad'])->name('agente.crear_novedad');
+    Route::get('novedad/avalatorios', [NovedadesController::class, 'getAvalatorios'])->name('novedad.avalatorios');
+    Route::get('agente/pdfHistorial', [AgenteController::class, 'imprimirHistorial'])->name('agente.pdf');
+    Route::post('agente/guardarNovedad', [NovedadesController::class, 'guardarNovedad'])->name('agente.crearNovedad');
+    Route::put('agente/modificarNovedad', [NovedadesController::class, 'modificarNovedad'])->name('agente.modificarNovedad');
+    Route::delete('agente/borrarNovedad', [NovedadesController::class, 'borrarNovedad'])->name('agente.borrarNovedad');
 });
