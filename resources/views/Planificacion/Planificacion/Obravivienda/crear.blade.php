eaa073b53eabcfe1f3cbd10f997c650bb9ba17f3
IprodhaNewWeb <80710956+Lisi-prog@users.noreply.github.com>
2023-09-11 11:38:55 -0300

    Update crear.blade.php

--- resources/views/Planificacion/Planificacion/Obravivienda/crear.blade.php
+++ resources/views/Planificacion/Planificacion/Obravivienda/crear.blade.php
@@ -92,7 +92,7 @@
                                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-3">
                                     <div class="form-group">
                                         {!! Form::label('Operatoria:', null, ['class' => 'control-label fs-6', 'style' => 'white-space: nowrap;']) !!}
-                                        <span class="obligatorio">*</span>
+                                        {{-- <span class="obligatorio">*</span> --}}
                                         {!! Form::select('idope', $TipoOpe, null, [
                                             'placeholder' => 'Seleccionar',
                                             'class' => 'form-select',
