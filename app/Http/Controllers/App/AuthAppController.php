d73f1bdbb18a482eeca830f87f46c6c08f1c338d
solmala <soledadmalazotto@gmail.com>
2023-09-12 08:56:04 -0300

    Update AuthAppController.php

--- app/Http/Controllers/App/AuthAppController.php
+++ app/Http/Controllers/App/AuthAppController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\App;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Iprodha\App_usuario;
