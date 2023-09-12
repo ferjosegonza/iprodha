64c54249cbf56eb2dcf3e85d5dd0c42b0d2ac334
solmala <92184025+solmala@users.noreply.github.com>
2023-09-12 08:49:50 -0300

    Revert "Revert "419 api login app produccion""

---
+++ app/Models/Iprodha/App_usuario.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models\Iprodha;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class App_usuario extends Model{
+
+    use HasFactory;
+    
+    public $timestamps = false;
+    
+    protected $table = 'iprodha.app_usuario';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [ 
+        'nombre', 'mail', 'usuario', 'contraseña', 'fecha_creacion'
+    ];
+
+    public function crear($nombre, $mail, $usuario, $contra){
+        date_default_timezone_set('America/Argentina/Buenos_Aires'); 
+        $this->nombre = $nombre;
+        $this->mail = $mail;
+        $this->usuario = $usuario;
+        $this->contraseña = password_hash($contra, 'PASSWORD_DEFAULT');
+        $this->fecha_creacion = date("Y-m-d h:i:s");
+        return $this->save();
+    }
+}
