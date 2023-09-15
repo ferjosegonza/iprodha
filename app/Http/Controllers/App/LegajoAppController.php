a53b6658071f982b47f4e59773420849e38a0134
solmala <soledadmalazotto@gmail.com>
2023-09-15 11:37:38 -0300

    Update LegajoAppController.php

--- app/Http/Controllers/App/LegajoAppController.php
+++ app/Http/Controllers/App/LegajoAppController.php
@@ -7,17 +7,17 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Iprodha\App_legajos;
 
-class AuthAppController extends Controller
+class LegajoAppController extends Controller
 {
     public function legajos(Request $request){
         //return response()->json($request);
-        /* $validator = Validator::make($request->all(), [
+        $validator = Validator::make($request->all(), [
             'cuil' => 'numeric',
         ]);
 
         if ($validator->fails()) {
             return response()->json(['error' => $validator->errors()], 400);
-        }  */
+        } 
         $legajos = App_legajos::where('cuil', '=', $request->cuil)->get();
         return response()->json($legajos);
     }
