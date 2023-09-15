5b4faf6446dd70cc34f9736bc51bec4046c1e2c6
Gabriel Dario Del Rosario <gddr85@hotmail.com>
2023-09-15 09:02:37 -0300

    nueva form licitacion

---
+++ app/Models/Iprodha/ob_licitacion.php
@@ -0,0 +1,13 @@
+<?php    
+    namespace App\Models\Iprodha;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Database\Eloquent\Model;    
+    class ob_licitacion extends model{
+        use HasFactory;
+        public$timestamps=false;        
+        protected$table='iprodha.ob_licitacion';
+        protected$primaryKey='ID_LICITACION';
+        public$incrementing=true;
+        protected$fillable=['ID_LICITACION','NUMERO','PATH','ID_TIPO','APERTURA','DENOMINACION','AÑO'];
+        public function ob_tipo_licitacion(){return$this->hasOne(ob_licitacion::class,'id_tipo','id_tipolic');}
+    }
---
+++ app/Models/Iprodha/ob_tipo_licitacion.php
@@ -0,0 +1,12 @@
+<?php    
+    namespace App\Models\Iprodha;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Database\Eloquent\Model;
+    class ob_licitacion extends model{
+        use HasFactory;
+        public$timestamps=false;        
+        protected$table='iprodha.ob_tipo_licitacion';
+        protected$primaryKey='ID_TIPOLIC';
+        public$incrementing=true;
+        protected$fillable=['ID_TIPOLIC','DESCRIPCION'];
+    }
