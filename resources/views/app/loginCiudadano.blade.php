bdb242f7a0d05c8ac8f8a0b62bd478fdd77ec864
solmala <soledadmalazotto@gmail.com>
2023-11-23 08:35:54 -0300

    Update loginCiudadano.blade.php

--- resources/views/app/loginCiudadano.blade.php
+++ resources/views/app/loginCiudadano.blade.php
@@ -1,4 +1,5 @@
 <head>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="{{asset('css/app/loginCiudadano.css')}}">
     <script src="{{ asset('js/app/loginCiudadano.js') }}"></script>
 </head>
