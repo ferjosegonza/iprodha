422720c3d7e77d4d4c5fc917ec558eb88fb22933
solmala <soledadmalazotto@gmail.com>
2023-11-23 08:59:32 -0300

    cambios login

--- app/Http/Controllers/App/AuthAppController.php
+++ app/Http/Controllers/App/AuthAppController.php
@@ -24,40 +24,32 @@ class AuthAppController extends Controller
     public function registerApp(Request $request){
         $validator = Validator::make($request->all(), [
             'nombre' => 'required|string|max:255',
-            'email' => 'required|string|email',
-            'contra' => 'required|string|min:6',
+            'email' => 'required|string|email'
         ]);
 
         if ($validator->fails()) {
             return response()->json(['error' => $validator->errors()], 400);
         }
-
-        $user = new App_usuario;
-        $user->crear($request->nombre, $request->email, $request->usuario, $request->contra);
-
-        return response()->json(['message' => 'User registered successfully']);
+        if(App_usuario::where('usuario', '=', $request->usuario)->first()){
+            return response()->json(['message' => 'User already registered']);
+        }
+        else{
+            $user = new App_usuario;
+            $user->crear($request->nombre, $request->email, $request->usuario);
+            return response()->json(['message' => 'User registered successfully']);
+        }
     }
 
     public function loginApp(Request $request){
         $validator = Validator::make($request->all(), [
-            'email' => 'required|string|email',
-            'contra' => 'required|string',
+            'usuario' => 'required'
         ]);
-
         if ($validator->fails()) {
             return response()->json(['error' => $validator->errors()], 400);
-        }   
-
-        $user = App_usuario::where('mail', '=', $request->email)->first();
-        $res = password_verify($request->contra, $user->contraseÑa);        
-        if($res){
-            $user->token = md5(uniqid().rand(1000000, 9999999));
-            $user->save();
-            return response()->json($user); 
         }
-        else{
-           return response()->json($res); 
-        }
-        
+        $user = App_usuario::where('usuario', '=', $request->usuario)->first();
+        $user->token = md5(uniqid().rand(1000000, 9999999));
+        $user->save();
+        return response()->json($user);
     }
 }
--- app/Models/Iprodha/App_usuario.php
+++ app/Models/Iprodha/App_usuario.php
@@ -15,7 +15,7 @@ class App_usuario extends Model{
     protected $primaryKey = 'id';
 
     protected $fillable = [ 
-        'nombre', 'mail', 'usuario', 'contraseña', 'fecha_creacion', 'token'
+        'nombre', 'mail', 'usuario', 'fecha_creacion', 'token'
     ];
 
     public function crear($nombre, $mail, $usuario, $contra){
@@ -23,7 +23,6 @@ class App_usuario extends Model{
         $this->nombre = $nombre;
         $this->mail = $mail;
         $this->usuario = $usuario;
-        $this->contraseña = password_hash($contra, PASSWORD_DEFAULT);
         $this->fecha_creacion = date("Y-m-d h:i:s");
         return $this->save();
     }
