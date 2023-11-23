5dea7e4d50bda4165f00d5a3bbc1354664c570b3
solmala <soledadmalazotto@gmail.com>
2023-11-23 09:17:12 -0300

    arreglar login app

--- public/js/app/loginCiudadano.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-    window.location.replace = "https://sso.misiones.gob.ar/auth/realms/Misiones/protocol/openid-connect/auth?response_type=code&client_id=iprodha-app-test&scope=email&redirect_uri=https%3A%2F%2Fsistema.iprodha.misiones.gob.ar%2Fiprodha-ciudadano";
-})
--- resources/views/app/loginCiudadano.blade.php
+++ resources/views/app/loginCiudadano.blade.php
@@ -1,4 +1,5 @@
 <head>
+    <meta http-equiv="refresh" content="0;url=https://sso.misiones.gob.ar/auth/realms/Misiones/protocol/openid-connect/auth?response_type=code&client_id=iprodha-app-test&scope=email&redirect_uri=https%3A%2F%2Fsistema.iprodha.misiones.gob.ar%2Fiprodha-ciudadano" />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="{{asset('css/app/loginCiudadano.css')}}">
     <script src="{{ asset('js/app/loginCiudadano.js') }}"></script>
