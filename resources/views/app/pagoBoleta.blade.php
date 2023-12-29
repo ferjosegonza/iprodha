33e9854d6977647d4b246acf838bc9f3c829b8d9
solmala <soledadmalazotto@gmail.com>
2023-12-29 10:22:32 -0300

    Update pagoBoleta.blade.php

--- resources/views/app/pagoBoleta.blade.php
+++ resources/views/app/pagoBoleta.blade.php
@@ -5,6 +5,22 @@
     <script src="{{ asset('js/app/pagoBoleta.js') }}"></script>
 </head> 
 <body> 
+    <style>
+        .loader {
+        border: 16px solid #f3f3f3; /* Light grey */
+        border-top: 16px solid #3498db; /* Blue */
+        border-radius: 50%;
+        width: 120px;
+        height: 120px;
+        animation: spin 2s linear infinite;
+        }
+
+        @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+        }
+    </style>
+    <div class="loader"></div>
 <form method="POST" action="https://sandboxpp.asjservicios.com.ar/" id="formPagos">
     <input type="hidden" name="CallbackSuccess" value="{{$postData['CallbackSuccess']}}" id="CallbackSuccess"/>
     <input type="hidden" name="CallbackCancel" value="{{$postData['CallbackCancel']}}" id="CallbackCancel"/>
